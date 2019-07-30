f18524c7bae78a8052451730d4dc30ac7a0ccde0
SaphiraTran <saphiratran@ccrmanager.com>
2019-07-30 08:01:39 +0800

    overall update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    ItemsComponent,
+    ItemDetailsComponent
   ],
   imports: [
     BrowserModule,
